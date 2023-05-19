96c88da34c9eff48646e52e00a427acca1e15a7f
devbylanre <devbylanre@gmail.com>
2023-05-19 10:52:43 +0100

    added the error and data result block

--- index.js
+++ index.js
@@ -1,52 +1,77 @@
-const searchField = document.querySelector('#searchField');
-const submitBtn = document.querySelector('#submitBtn');
-
+const searchField = document.querySelector("#searchField");
+const submitBtn = document.querySelector("#submitBtn");
 
 //declare errors
 const setError = (error) => {
-	return error;
-	console.log(error)
-}
+  console.log(error);
+  return error;
+};
 
 //fetch data
 const fetchData = (url) => {
-	document.querySelector('#preloader').classList.add('flex')
+  document.querySelector("#preloader").classList.add("flex");
 
-	fetch(url)
-	.then(response => {
-		response.ok 
-		? (
-			document.querySelector('#preloader').classList.remove('flex'), 
-			console.log(response.json())
-		)
-		: (
-			console.log('error fetching', searchField.value), 
-			document.querySelector('#preloader').classList.remove('flex') 
-		)
-	})
-	.catch(err => {
-		//outputs error functions
-		console.log(searchField.value, err)
-		setTimeout(() => document.querySelector('#preloader').classList.remove('flex'), 4000) 
-	})
+  fetch(url)
+    .then((response) => {
+      response.ok
+        ? (
+            setTimeout(() => document.querySelector("#preloader").classList.remove("flex"), 4000),
+            displayResults(response.json())
+          )
+        : (
+            setTimeout(() => document.querySelector("#preloader").classList.remove("flex"), 6000),
+            setError('Error occured while fetching results')
+          ) 
+    })
+    .catch((error) => {
+      setTimeout(() => document.querySelector("#preloader").classList.remove("flex"),
+        6000
+      );
+      //outputs error functions
+      setError('Unable to fetch meaning of', searchField.value)
+    });
+};
 
+
+const setSynonym = () => {
+  const relatedWord = document.querySelector('#related').innerText;
+  searchField.value = relatedWord;
+  //api endpoint
+  const api =
+    "https://api.dictionaryapi.dev/api/v2/entries/en/" + searchField.value;
+
+  //regex expression
+  const inputRegex = /[A-z]$/;
+
+  
+  inputRegex.test(searchField.value)
+    ? (setError(''),
+      //fecthes data
+      fetchData(api))
+    : setError("invalid characters");
 }
 
+document.querySelector('#related').addEventListener('click', setSynonym);
 
-submitBtn.addEventListener('click', () => {
-	//api endpoint
-	const api = 
-	'https://api.dictionaryapi.dev/api/v2/entries/en/' + searchField.value;
 
-	//regex expression
-	const inputRegex = /[A-z]$/;
 
-	(inputRegex.test(searchField.value)) 
-	? (
-		console.log(searchField.value),
-		//fecthes data
-		fetchData(api)
-	)
-	: console.log(setError('invalid characters'))
-	
-})
+// Display results to console.
+const displayResults = (results) => { 
+  console.log(results)
+}
+
+submitBtn.addEventListener("click", () => {
+  //api endpoint
+  const api =
+    "https://api.dictionaryapi.dev/api/v2/entries/en/" + searchField.value;
+
+  //regex expression
+  const inputRegex = /[A-z]$/;
+
+  
+  inputRegex.test(searchField.value)
+    ? (setError(''),
+      //fecthes data
+      fetchData(api))
+    : setError("invalid characters");
+});
